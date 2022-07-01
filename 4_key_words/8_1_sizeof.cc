2134f5934f2df33dbbf9109072558831a965530a
wangyunlong01 <wangyunlong01@inspur.com>
2022-07-01 16:13:04 +0800

    feat: add common header file

---
+++ 4_key_words/8_1_sizeof.cc
@@ -0,0 +1,9 @@
+//
+// Created by wangyunlong01 on 2022/7/1.
+//
+
+#include "common.h"
+
+int main() {
+    cout << sizeof(char) << endl;
+}
---
+++ common.h
@@ -0,0 +1,11 @@
+//
+// Created by wangyunlong01 on 2022/7/1.
+//
+
+#ifndef HELLOC_COMMON_H
+#define HELLOC_COMMON_H
+
+#endif //HELLOC_COMMON_H
+
+#include <iostream>
+using namespace std;
