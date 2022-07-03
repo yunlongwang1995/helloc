4094a2c88658d965c7cede44e20ca2a15ae30946
wangyunlong01 <wangyunlong01@inspur.com>
2022-07-03 10:19:41 +0800

    feat: add backtracking case

---
+++ 3_algorithm/leetcode/078_subsets.cc
@@ -0,0 +1,19 @@
+/**
+ *
+ *
+ */
+
+#include <iostream>
+#include <vector>
+#include <string>
+#include <queue>
+#include <algorithm>
+using namespace std;
+
+vector<vector<int>> subsets(vector<int>& nums) {
+
+}
+
+int main() {
+
+}
---
+++ 3_algorithm/leetcode/079_exist.cc
@@ -0,0 +1,20 @@
+/**
+ *
+ *
+ *
+ */
+
+#include <iostream>
+#include <vector>
+#include <string>
+#include <queue>
+#include <algorithm>
+using namespace std;
+
+bool exist(vector<vector<char>>& board, string word) {
+
+}
+
+int main() {
+
+}
