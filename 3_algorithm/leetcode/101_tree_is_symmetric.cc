2e38a7ab8057cbc59cc0b1083dea147bd9bf88ec
wangyunlong01 <wangyunlong01@inspur.com>
2022-08-08 20:50:14 +0800

    feat: 完善数算法

--- 3_algorithm/leetcode/101_tree_is_symmetric.cc
+++ 3_algorithm/leetcode/101_tree_is_symmetric.cc
@@ -4,40 +4,35 @@
  *
  */
 
-#include <iostream>
-#include <vector>
-#include <string>
-#include <queue>
-#include <algorithm>
 #include "../../common.h"
 
-bool isSymmetricHelp(TreeNode* left, TreeNode* right) {
-  if (left == nullptr && right == nullptr) {
-    return true;
-  }
-  if (left == nullptr) {
-    return false;
-  }
-  if (right == nullptr) {
-    return false;
-  }
-  if (left->val != right->val) {
-    return false;
-  }
+bool isSymmetricHelp(TreeNode *left, TreeNode *right) {
+    if (left == nullptr && right == nullptr) {
+        return true;
+    }
+    if (left == nullptr) {
+        return false;
+    }
+    if (right == nullptr) {
+        return false;
+    }
+    if (left->val != right->val) {
+        return false;
+    }
 
-  if (isSymmetricHelp(left->left, right->right) && isSymmetricHelp(left->right, right->left)) {
-    return true;
-  }
+    if (isSymmetricHelp(left->left, right->right) && isSymmetricHelp(left->right, right->left)) {
+        return true;
+    }
 
-  return false;
+    return false;
 }
 
-bool isSymmetric(TreeNode* root) {
-  if (root == nullptr) {
-    return true;
-  }
+bool isSymmetric(TreeNode *root) {
+    if (root == nullptr) {
+        return true;
+    }
 
-  return isSymmetricHelp(root->left, root->right);
+    return isSymmetricHelp(root->left, root->right);
 }
 
 
--- 3_algorithm/leetcode/121_removeRepeatedNum.cc
+++ 3_algorithm/leetcode/121_removeRepeatedNum.cc
@@ -4,80 +4,75 @@
  *
  */
 
-#include <iostream>
-#include <vector>
-#include <string>
-#include <queue>
-#include <algorithm>
 #include "../../common.h"
 
-ListNode* removeRepeatedNum(ListNode* head) {
-  if (head == nullptr) {
-    return nullptr;
-  }
-
-  ListNode* newHead = nullptr;
-  ListNode* left = nullptr;
-  ListNode* middle = nullptr;
-  ListNode* right = nullptr;
-  ListNode* p = head->next;
-  int count = 0;
-
-  //1: find new head node
-  while (true) {
-    if (p == nullptr) {
-      if (count == 0) {
-        return head;
-      }
-      return nullptr;
-    }
-    if (head->val == p->val) {
-      p = p->next;
-      count++;
-      continue;
+ListNode *removeRepeatedNum(ListNode *head) {
+    if (head == nullptr) {
+        return nullptr;
     }
 
-    if (count == 0) {
-      newHead = head;
-      left = head;
-      middle = p;
-      left->next = middle;
-      break;
-    }
-    head = p;
-    p = head->next;
-  }
+    ListNode *newHead = nullptr;
+    ListNode *left = nullptr;
+    ListNode *middle = nullptr;
+    ListNode *right = nullptr;
+    ListNode *p = head->next;
+    int count = 0;
 
-  //2: remove repeated node
-  right = middle->next;
-  count = 0;
-  while (true) {
-    if (right == nullptr) {
-      if (count != 0) {
-        left->next = nullptr;
-      }
-      break;
-    }
+    //1: find new head node
+    while (true) {
+        if (p == nullptr) {
+            if (count == 0) {
+                return head;
+            }
+            return nullptr;
+        }
+        if (head->val == p->val) {
+            p = p->next;
+            count++;
+            continue;
+        }
 
-    if (middle->val == right->val) {
-      right = right->next;
-      count++;
-      continue;
-    }
-
-    if (count == 0) {
-      left = middle;
-      middle = right;
-      left->next = middle;
-      continue;
+        if (count == 0) {
+            newHead = head;
+            left = head;
+            middle = p;
+            left->next = middle;
+            break;
+        }
+        head = p;
+        p = head->next;
     }
 
-    middle = right;
-    left->next = middle;
+    //2: remove repeated node
     right = middle->next;
-  }
+    count = 0;
+    while (true) {
+        if (right == nullptr) {
+            if (count != 0) {
+                left->next = nullptr;
+            }
+            break;
+        }
 
-  return newHead;
+        if (middle->val == right->val) {
+            right = right->next;
+            count++;
+            continue;
+        }
+
+        if (count == 0) {
+            left = middle;
+            middle = right;
+            left->next = middle;
+            continue;
+        }
+
+        middle = right;
+        left->next = middle;
+        right = middle->next;
+    }
+
+    return newHead;
 }
 
 int main() {
