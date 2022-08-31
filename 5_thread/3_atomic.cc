a9f86f4a7b977d74e46fc24bda3b81830ee44336
wangyunlong01 <wangyunlong01@inspur.com>
2022-08-31 14:44:26 +0800

    feat: add atomic test

--- 5_thread/3_atomic.cc
+++ 5_thread/3_atomic.cc
@@ -1,4 +1,13 @@
 //
 // Created by wangyunlong01 on 2022/6/22.
 //
+#include <atomic>
+#include <iostream>
+using namespace std;
 
+int main() {
+  atomic<int> num;
+  num.fetch_add(50);
+  int res = num.fetch_add(50);
+  cout << res << endl;
+}
