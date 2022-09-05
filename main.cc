abfcf623f643228898535bdb514c20114daf248c
wangyunlong01 <wangyunlong01@inspur.com>
2022-09-05 11:17:37 +0800

    feat: add cmd param

--- main.cc
+++ main.cc
@@ -3,6 +3,11 @@
 //
 #include <iostream>
 
-int main(int argc, char **argv) {
-    std::cout << "hello c++ world" << std::endl;
+int main(int argc, char* argv[]) {
+  std::cout << "hello c++ world" << std::endl;
+
+  auto param1 = argv[0];
+  auto param2 = argv[1];
+
+  std::cout << param1 << "\n" << param2 << std::endl;
 }
