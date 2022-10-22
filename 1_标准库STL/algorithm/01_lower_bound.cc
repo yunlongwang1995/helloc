9cd58c6f075177dac9c425959b232ec854bfffe8
wangyunlong01 <wangyunlong01@inspur.com>
2022-10-22 11:04:59 +0800

    feat: lower_bound test

--- 1_标准库STL/algorithm/01_lower_bound.cc
+++ 1_标准库STL/algorithm/01_lower_bound.cc
@@ -16,18 +16,41 @@
 using namespace std;
 
 bool mycmp(int i, int j) {
-    return i > j;
+  return i < j;
+}
+
+class Person {
+public:
+  Person(int age, string name) : age_(age), name_(name) {}
+
+public:
+  int age_;
+  string name_;
+};
+
+bool comp2(Person l, Person r) {
+  return l.age_ < r.age_;
 }
 
 int main() {
 
-    // 在 [first, last) 区域内查找不小于 val 的元素
-    vector<int> values = {1,3,5,9,10};
-    auto p = lower_bound(values.begin(), values.end(), 5);
-    cout << "p = " << *p << endl;
+  // 在 [first, last) 区域内查找不小于 val 的元素
+  vector<int> values = {1, 3, 5, 9, 10};
+  auto p = lower_bound(values.begin(), values.end(), 5);
+  cout << "p = " << *p << endl;
 
-    // 在 [first, last) 区域内查找第一个不符合 comp 规则的元素
-    vector<int> values2 = {15, 12,6,10,5};
-    auto p2 = lower_bound(values2.begin(), values2.end(), 7, mycmp);
-    cout << "p2 = " << *p2 << endl;
+  vector<Person> values3;
+  values3.emplace_back(Person(1, ""));
+  values3.emplace_back(Person(3, ""));
+  values3.emplace_back(Person(5, ""));
+  values3.emplace_back(Person(7, ""));
+  values3.emplace_back(Person(9, ""));
+  values3.emplace_back(Person(11, ""));
+
+  auto iter = lower_bound(values3.begin(), values3.end(), Person(4, "w"), comp2);
+  if (iter == values3.end()) {
+    cout << "not found..." << endl;
+  } else {
+    cout << iter.base()->age_ << endl;
+  }
 }
