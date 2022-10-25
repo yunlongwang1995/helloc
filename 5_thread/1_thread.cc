2ae188829e185b67d5e13220dac8090b540e43e4
wangyunlong01 <wangyunlong01@inspur.com>
2022-10-25 09:28:17 +0800

    feat: 多线程 test

--- 5_thread/1_thread.cc
+++ 5_thread/1_thread.cc
@@ -9,19 +9,18 @@
 using namespace std;
 
 void printNum(int num) {
+  this_thread::sleep_for(std::chrono::seconds(num));  // Sleep 1s
   cout << "threadID: " << this_thread::get_id() << ", num: " << num << endl;
   this_thread::yield();   // 让出时间片
-  this_thread::sleep_for(std::chrono::seconds(1));  // Sleep 1s
 }
 
 int main() {
   std::thread threads[2];
-//  std::vector<std::thread> threads;
-  {
-    thread(printNum, 1000).detach();
-  }
-//  threads[0].join();        // 主线程等待
-//  threads[0].detach();      // 与主线程分离
+  threads[0] = thread(printNum, 1);
+  threads[1] = thread(printNum, 5);
+
+  threads[0].join();        // 主线程等待
+  threads[1].detach();      // 与主线程分离
 
   cout << "main thread：" << this_thread::get_id() << endl;
   this_thread::sleep_for(chrono::seconds(2));
