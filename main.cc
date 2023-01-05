a70371c383710dfa4cf2a1a1da588982a44fd48a
wangyunlong01 <wangyunlong01@inspur.com>
2023-01-05 11:31:15 +0800

    feat: 入门用例

--- main.cc
+++ main.cc
@@ -3,11 +3,19 @@
 //
 #include <iostream>
 
+/**
+ * 入门用例
+ * @param argc 　　参数个数 3
+ * @param argv 　　参数列表 ./main hello 123
+ * @return
+ */
 int main(int argc, char* argv[]) {
   std::cout << "hello c++ world" << std::endl;
 
   auto param1 = argv[0];
   auto param2 = argv[1];
 
-  std::cout << param1 << "\n" << param2 << std::endl;
+  std::cout <<" argc: " << argc << std::endl;
+  std::cout <<" param1: " << param1 << std::endl;
+  std::cout <<" param2: " << param2 << std::endl;
 }
