1bc723b56e4b30e44b151126ba309960fb5045cc
yunlongwang1995 <525081724@qq.com>
2023-01-11 00:18:10 +0800

    feat: leetcode 011

---
+++ 3_algorithm/leetcode/011_max_area.cc
@@ -0,0 +1,19 @@
+/**
+ * 给定一个长度为 n 的整数数组height。有n条垂线，第 i 条线的两个端点是(i, 0)和(i, height[i])。
+ * 找出其中的两条线，使得它们与x轴共同构成的容器可以容纳最多的水。
+ * 返回容器可以储存的最大水量。
+ *
+ */
+
+#include <iostream>
+#include <string>
+#include <vector>
+using namespace std;
+
+int maxArea(vector<int>& height) {
+
+}
+
+int main() {
+
+}
