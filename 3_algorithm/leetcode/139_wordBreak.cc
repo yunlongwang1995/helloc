911c5682ca15cd3d6683f0afc97772e51bcd0461
wangyunlong01 <wangyunlong01@inspur.com>
2023-01-16 09:45:14 +0800

    feat: 139 word break

--- 3_algorithm/leetcode/139_wordBreak.cc
+++ 3_algorithm/leetcode/139_wordBreak.cc
@@ -6,10 +6,59 @@
 
 #include "../../base.h"
 
-bool wordBreak(string s, vector<string>& wordDict) {
+bool dfs(const string& s, const unordered_map<string, bool>& dict, int startIndex) {
+  // 基准情况
+  if (startIndex == s.length()) {
+    return true;
+  }
 
+  // 遍历
+  for (int i=startIndex+1; i<s.length(); ++i) {
+    string subString = s.substr(startIndex, i);
+    if (dict.at(subString)) {
+      return dfs(s, dict, i);
+    }
+  }
+
+  return false;
+}
+
+// method1: backtracing
+bool wordBreak(string s, vector<string>& wordDict) {
+  unordered_map<string, bool> dict;
+  for (auto item: wordDict) {
+    dict[item] = true;
+  }
+  return dfs(s, dict, 0);
 }
 
 int main() {
 
+}
+
+// method2: dp
+bool wordBreak2(string s, vector<string>& wordDict) {
+  // 初始化
+  bool dp[s.length()+1];
+  for (int i=0; i<=s.length(); ++i) {
+    dp[i] = false;
+  }
+  dp[0] = true;
+
+  unordered_map<string, bool> dict;
+  for (auto item: wordDict) {
+    dict[item] = true;
+  }
+
+  for (int i=1; i<=s.length(); ++i) {
+    for (int j=i; j>=0; --j) {
+      string subString = s.substr(j, i+1);
+      if (dict.at(subString) && dp[j-1]) {
+        dp[i] = true;
+        break;
+      }
+    }
+  }
+
+  return dp[s.length()-1];
 }
