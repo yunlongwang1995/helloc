eb9bcecbaa11d8f770f06fda8ae3d78d52650be0
wangyunlong01 <wangyunlong01@inspur.com>
2023-02-01 20:49:19 +0800

    feat: 添加 macro 测试

--- 3_algorithm/leetcode/000_huawei3.cc
+++ 3_algorithm/leetcode/000_huawei3.cc
@@ -2,6 +2,43 @@
 
 using namespace std;
 
+
+int cas(int expect_value, int new_value, int* val) {
+
+}
+
+class SpinLock {
+ public:
+  void Lock() {
+    if(cas(0, 1, &lockd_value) == 0) {
+      return;
+    }
+
+    while (true) {
+      if (cas(0, 1, &lockd_value) == 0) {
+        break;
+      }
+    }
+  }
+
+  void UnLock() {
+    if(cas(1, 0, &lockd_value) == 1) {
+      return;
+    }
+
+    // 是否 panic
+
+  }
+
+ private:
+  int lockd_value = 0;
+};
+
+
+
+
 int main(int argc, char* argv[]) {
   cout << "hello world" << endl;
+
+
 }
