13159b19049d2b133baa4075d5380389a5774708
wangyunlong01 <wangyunlong01@inspur.com>
2023-03-24 09:48:14 +0800

    refactor: 重构 0324

--- 8_key_words/0_0_macro.cc
+++ 8_key_words/0_0_macro.cc
@@ -2,10 +2,36 @@
 
 
 #define TOSTR1(_FILENAME_) #_FILENAME_
-#define TOSTR2(_FILENAME_,_SUFFIX_) #_FILENAME_"."#_SUFFIX_
+#define TOSTR2(_FILENAME_, _SUFFIX_) #_FILENAME_"."#_SUFFIX_
+
+/**
+ * #: 转字符串
+ */
+void ToStringTest();
+
+
+#define IdentCAT(_IDT1_, _IDT2_) my_##_IDT1_##_##_IDT2_
+#define TOSTR(_NAME_) #_NAME_" = "
+
+/**
+ * ##: 转标识符
+ */
+void IdentCatTest();
 
 int main() {
+  ToStringTest();
+  cout << "----------------------" << endl;
+  IdentCatTest();
+}
+
+void ToStringTest() {
   cout << TOSTR1(cloud) << endl;
   cout << TOSTR2(cloud, txt) << endl;
-  return 0;
+}
+
+void IdentCatTest() {
+  int IdentCAT(first, bag) = 1;
+  int IdentCAT(second, bag) = 18;
+  cout << TOSTR(my_first_bag) << IdentCAT(first, bag) << endl;
+  cout << TOSTR(my_second_bag) << IdentCAT(second, bag) << endl;
 }
--- 8_key_words/0_2_string.cc
+++ 8_key_words/0_2_string.cc
@@ -2,41 +2,81 @@
 // Created by wangyunlong01 on 2022/6/27.
 //
 
-#include <string>
+#include <cstring>
 #include <iostream>
 
 using namespace std;
 
+/**
+ * 地址测试
+ */
+void AddrTest();
+
+void AssignmentTest();
+
+/**
+ * string 库常用方法测试
+ */
+void CommonMethodTest();
+
 int main() {
-    string val = "(())";
-    // 1. c_str
-    const char *p = val.c_str();
-    cout << "c_str():\n" << *p << endl;
-    p++;
-    cout << *p << endl;
-    p += 2;
-    cout << (*p == 0) << endl;
+  AddrTest();
+  cout << "-------------------------" << endl;
+  AssignmentTest();
+  cout << "-------------------------" << endl;
+  CommonMethodTest();
+}
 
-    // 2. data()
-    const char *p2 = val.data();
-    cout << "data():\n" << *p2 << endl;
-    p2++;
-    cout << *p2 << endl;
-    p2 += 2;
-    cout << (*p2 == 0) << endl;
+void AddrTest() {
+  string s1 = "hello";
+  cout << &s1 << endl;
+  s1 += " world";
+  cout << &s1 << endl;
 
-    // 3. find
-    int idx = val.find(")*", 0);
-    cout << "first: " << (idx) << endl;
+  cout << s1[1] << endl;
+  cout << &s1[1] << endl;
+}
 
-    // 4. find_first_of
-    cout << "find_first_of: " << val.find_first_of("()", 0) << endl;
+void AssignmentTest() {
+  string s1 = "hello world ";
+  string s2 = s1;
+  cout << &s1 << endl;
+  cout << &s2 << endl;
 
-    // 5. sub_str
-    cout << "sub_str: " << (val.substr(0, 2) == "((") << endl;
+  const char *p = s2.c_str();
+  cout << strlen(p) << endl;
+}
 
-    // 6. replace
-    val = ".....";
-    val.replace(3, 1, "Q");
-    cout << val << endl;
+void CommonMethodTest() {
+  string val = "(())";
+  // 1. c_str
+  const char* p = val.c_str();
+  cout << "c_str():\n" << *p << endl;
+  p++;
+  cout << *p << endl;
+  p += 2;
+  cout << (*p == 0) << endl;
+
+  // 2. data()
+  const char* p2 = val.data();
+  cout << "data():\n" << *p2 << endl;
+  p2++;
+  cout << *p2 << endl;
+  p2 += 2;
+  cout << (*p2 == 0) << endl;
+
+  // 3. find
+  int idx = val.find(")*", 0);
+  cout << "first: " << (idx) << endl;
+
+  // 4. find_first_of
+  cout << "find_first_of: " << val.find_first_of("()", 0) << endl;
+
+  // 5. sub_str
+  cout << "sub_str: " << (val.substr(0, 2) == "((") << endl;
+
+  // 6. replace
+  val = ".....";
+  val.replace(3, 1, "Q");
+  cout << val << endl;
 }
